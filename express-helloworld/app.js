cf527e65d4d204788052a063973d714e0e5c86d7
Miguel Gordillo <miquel.enric.gordillo@gmail.com>
2024-04-05 10:27:51 +0200

    ss

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -2,8 +2,8 @@ var express = require('express');
 app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello Quick change!\n');
-});c
+  res.send('Hello Quick change!!\n');
+});
 
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
